91ad4f6045078c761d0e7239d3d88640de5f0416
Oleg Gavrilenkov <openzone@yandex.ru>
2016-08-27 19:00:33 +0300

    Add files via upload

---
+++ OMInsurance.WebApps/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using OMInsurance.WebApps.Security;
+using System.Web.Mvc;
+
+namespace OMInsurance.WebApps
+{
+    public static class FilterConfig
+    {
+        internal static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new AuthorizeUserAttribute());
+        }
+    }
+}
---
+++ OMInsurance.WebApps/App_Start/RouteConfig.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace OMInsurance.WebApps
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+            routes.MapRoute(
+                name: "Files",
+                url: "File/Image/{filename}",
+                defaults: new { controller = "File", action = "Image", filename = UrlParameter.Optional }
+            );
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "ClientVisit", action = "Actuals", id = UrlParameter.Optional }
+            );
+            routes.MapRoute(
+                name: "Login",
+                url: "Account/Login",
+                defaults: new { controller = "Account", action = "Login" }
+            );
+            routes.MapRoute(
+                name: "PrintedForms",
+                url: "PrintedForms/{action}/{id}",
+                defaults: new { controller = "PrintedForms", action = "ChangeInsuranceCompanyApplication", id = UrlParameter.Optional }
+            );
+
+        }
+    }
+}
